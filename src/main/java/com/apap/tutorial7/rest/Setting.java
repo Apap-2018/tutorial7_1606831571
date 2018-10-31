24c55d2d2268665ccff6b6c8a8e776c7a5e2be46
raiindhira <raiindhirasaraswati@gmail.com>
2018-10-31 19:13:33 +0700

    nomor 2 masih error forbiden 403

--- src/main/java/com/apap/tutorial7/rest/Setting.java
+++ src/main/java/com/apap/tutorial7/rest/Setting.java
@@ -1,6 +1,7 @@
 package com.apap.tutorial7.rest;
 
-public class Setting {
+public class Setting { //link sudah tetapz
 	final public static String dealerUrl = "https://ae37a582-f627-4a66-96c1-6b391d6ddc59.mock.pstmn.io\r\n";
-
+	final public static String produsenUrl = "https://www.carqueryapi.com/api/0.3/?cmd=getModels";
+//make dirubah-rubah sehingga ditaro dicontroller ajah
 }
