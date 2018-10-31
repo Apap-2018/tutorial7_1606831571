ee0c3df19d38527f933835f96f9133d7200a6881
raiindhira <raiindhirasaraswati@gmail.com>
2018-10-31 19:31:34 +0700

    Nomor 2 done+year tahun ini juga udah selesai

--- src/main/java/com/apap/tutorial4/controller/ProdusenController.java
+++ src/main/java/com/apap/tutorial4/controller/ProdusenController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
+import java.util.Calendar;
 import com.apap.tutorial7.rest.Setting;
 
 @RestController
@@ -23,7 +23,8 @@ public class ProdusenController {
 
 	@GetMapping(value = "/model")
 	 private  ResponseEntity<String> getStatus(@RequestParam("factory") String namaProdusen) throws Exception{
-	  String path = Setting.produsenUrl + "&make=" + namaProdusen;
+	int year = Calendar.getInstance().get(Calendar.YEAR);
+      String path = Setting.produsenUrl + "&make=" + namaProdusen + "&year=" + year;
 	  System.out.println(path);
 	  ResponseEntity<String> response = restTemplate.getForEntity(path, String.class);
 	  return response;
